1bfa28b17c8ab70d683951b6837735638582ae76
Inkognita <liepieshov@ucu.edu.ua>
2017-06-06 07:21:27 +0300

    Added the third function in read_from_files to read from both files at one time

--- facebook_network_graph/read_from_files.py
+++ facebook_network_graph/read_from_files.py
@@ -30,10 +30,36 @@ def read_from_files2(file_data, path_to_files):
     graph = NetworkGraph(file_name="interested.db")
     graph.clear()
     for name, fb_id in data:
-        graph.add_node(name=name, facebook_id=graph.id_from_url(fb_id)).facebook_id
+        graph.add_node(name=name, facebook_id=graph.id_from_url(fb_id))
     for name, fb_id in data:
         new_user = graph.findNode(name=name, facebook_id=graph.id_from_url(fb_id))
         graph.read_friends_from_file(new_user, "%s%s.txt" % (path_to_files, name), adding_new=False)
 
-read_from_files2("/home/liepieshov/CourseWork/data/interested.txt",
-                 "/home/liepieshov/CourseWork/db_interested/")
+
+def read_from_files3(file_data_lst, path_to_files_lst):
+
+    graph = NetworkGraph(file_name="gathered.db")
+    graph.clear()
+
+    data1 = read_file(file_data_lst[0])
+    for name, fb_id in data1:
+        graph.add_node(name=name, facebook_id=graph.id_from_url(fb_id))
+
+    data2 = read_file(file_data_lst[1])
+    for name, fb_id in data2:
+        graph.add_node(name=name, facebook_id=graph.id_from_url(fb_id))
+
+    for name, fb_id in data1:
+        new_user = graph.findNode(name=name, facebook_id=graph.id_from_url(fb_id))
+        graph.read_friends_from_file(new_user, "%s%s.txt" % (path_to_files_lst[0], name), adding_new=False)
+
+    for name, fb_id in data2:
+        new_user = graph.findNode(name=name, facebook_id=graph.id_from_url(fb_id))
+        graph.read_friends_from_file(new_user, "%s%s.txt" % (path_to_files_lst[1], name), adding_new=False)
+
+# read_from_files2("/home/liepieshov/CourseWork/data/interested.txt",
+#                  "/home/liepieshov/CourseWork/db_interested/")
+read_from_files3(["/home/liepieshov/CourseWork/data/interested.txt",
+                  "/home/liepieshov/CourseWork/data/will_go_lst.txt"],
+                 ["/home/liepieshov/CourseWork/db_interested/",
+                  "/home/liepieshov/CourseWork/db/"])
